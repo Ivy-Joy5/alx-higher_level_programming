4acf30f184c69cd049db2c39cd14a18830cd041a
Ivy-Joy5 <chumbaivyjoy@gmail.com>
2024-10-23 07:23:27 -0700

    A square is a rectangle

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,77 +1,68 @@
 #!/usr/bin/python3
-"""Solves the N-Queens puzzle using a non-recursive approach."""
+"""Solves the N Queens problem."""
+import sys
 
-from sys import argv
 
-if len(argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
+def print_solution(solution):
+    """Prints the board's solution in the required format."""
+    formatted_solution = []
+    for i in range(len(solution)):
+        formatted_solution.append([i, solution[i]])
+    print(formatted_solution)
 
-if not argv[1].isdigit():
-    print('N must be a number')
-    exit(1)
 
-N = int(argv[1])
+def is_safe(board, row, col, N):
+    """Checks if it's safe to place a queen at board[row][col]."""
+    # Check this row on left side
+    for i in range(col):
+        if board[row] == i:
+            return False
 
-if N < 4:
-    print('N must be at least 4')
-    exit(1)
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i] == j:
+            return False
 
-def board_column_gen(board=[]):
-    """Adds a column of zeroes to the right of the board."""
-    if len(board):
-        for row in board:
-            row.append(0)
-    else:
-        for row in range(N):
-            board.append([0])
-    return board
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, N), range(col, -1, -1)):
+        if board[i] == j:
+            return False
 
-def add_queen(board, row, col):
-    """Places a queen (1) on the board at the given row and column."""
-    board[row][col] = 1
-
-def new_queen_safe(board, row, col):
-    """Checks if a queen can be placed at the given position without conflicts."""
-    for i in range(1, N):
-        if (col - i) >= 0:
-            if (row - i) >= 0 and board[row - i][col - i]:  # up-left diagonal
-                return False
-            if board[row][col - i]:  # left
-                return False
-            if (row + i) < N and board[row + i][col - i]:  # down-left diagonal
-                return False
     return True
 
-def coordinate_format(candidates):
-    """Converts the board matrix into a list of queen positions."""
-    formatted = []
-    for attempt in candidates:
-        result = []
-        for i, row in enumerate(attempt):
-            for j, col in enumerate(row):
-                if col:
-                    result.append([i, j])
-        formatted.append(result)
-    return formatted
 
-# Initialize the candidates with the first column of zeroes
-candidates = [board_column_gen()]
+def solve_nqueens_util(board, col, N):
+    """Utilizes backtracking to find all solutions."""
+    if col == N:
+        print_solution(board)
+        return
 
-# Test column by column
-for col in range(N):
-    new_candidates = []
-    for matrix in candidates:
-        for row in range(N):
-            if new_queen_safe(matrix, row, col):
-                temp = [line[:] for line in matrix]
-                add_queen(temp, row, col)
-                if col < N - 1:
-                    board_column_gen(temp)
-                new_candidates.append(temp)
-    candidates = new_candidates
+    for i in range(N):
+        if is_safe(board, i, col, N):
+            board[i] = col
+            solve_nqueens_util(board, col + 1, N)
+            board[i] = -1  # Backtrack
 
-# Print the results in the required format
-for solution in coordinate_format(candidates):
-    print(solution)
 
+def solve_nqueens(N):
+    """Solves the N Queens problem using backtracking."""
+    board = [-1] * N
+    solve_nqueens_util(board, 0, N)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(sys.argv[1])
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solve_nqueens(N)
