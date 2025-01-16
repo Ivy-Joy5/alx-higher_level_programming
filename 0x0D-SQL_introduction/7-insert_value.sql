673f26bedd0dbbab612a7052a23a56ccabc9eafe
Ivy-Joy5 <chumbaivyjoy@gmail.com>
2025-01-16 14:23:50 -0800

    First add

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Insert a new row into the first_table
+-- The new row will have id = 89 and name = 'Best School'
+-- If this script is run multiple times, it will insert multiple identical rows
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
