255d5598486825210be1a60ebf7adc24e1f03f44
Ivyjoy Jepchumba <chumbaivyjoy@gmail.com>
2025-01-24 01:12:26 +0300

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,2 @@
 #!/usr/bin/node
-
-const arg = process.argv[2];
-
-if (isNaN(arg)) {
-	  console.log('Not a number');
-} else {
-	  console.log('My number: ' + parseInt(arg));
-}
-
+console.log(parseInt(process.argv[2]) ? `My number: ${parseInt(process.argv[2])}` : 'Not a number');
