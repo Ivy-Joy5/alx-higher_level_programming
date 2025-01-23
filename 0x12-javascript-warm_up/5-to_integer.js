ef7b8b7e1268f445c17763748bf47495aa64281c
Ivyjoy Jepchumba <chumbaivyjoy@gmail.com>
2025-01-24 01:04:11 +0300

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,8 +3,8 @@
 const arg = process.argv[2];
 
 if (isNaN(arg)) {
-	console.log('Not a number');
-
-}else{
-	console.log('My number:' + parsInt(arg));
+	  console.log('Not a number');
+} else {
+	  console.log('My number: ' + parseInt(arg));
 }
+
